a97ce9c4f26520d3be3d39a3d41efa50727f077c
Sibusiso <31421738+SibusisoShabalala@users.noreply.github.com>
2021-04-17 23:30:19 +0200

    Update E_EpidemicRouter.java

--- src/routing/E_EpidemicRouter.java
+++ src/routing/E_EpidemicRouter.java
@@ -1,7 +1,7 @@
 /* 
  * Copyright 2010 Aalto University, ComNet
  *Edited by Sibusiso Shabalala (to make it Energy Efficient Routing Protocol) 
- * Released under GPLv3. See LICENSE.txt for details. 
+ * Released under GPLv3. See LICENSE.txt for details. j
  */
 package routing;
 
